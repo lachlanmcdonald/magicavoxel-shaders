4c913851166fe0f8ffc92319fe62a55769758171
Lachlan McDonald <lachlanmcdonald@users.noreply.github.com>
2021-06-25 14:12:18 +0100

    RF python scripts

--- update.py
+++ update.py
@@ -1,7 +1,6 @@
 from os import path
 from datetime import datetime
 import yaml
-import re
 
 DIR = path.dirname(path.realpath(__file__))
 ARG_FORMAT = "{} = '{}'"
@@ -21,6 +20,9 @@ with open(path.join(DIR, 'shaders.yml')) as f:
 
 # Update shaders
 for shader_name in params.keys():
+	print("Updating {}".format(shader_name))
+
+	# Updated copyright header
 	header = [
 		'MIT License (MIT)',
 		'https://github.com/lachlanmcdonald/magicavoxel-shaders',
@@ -28,9 +30,11 @@ for shader_name in params.keys():
 		'',
 	]
 
+	# Console command instructions
 	param_strings = ' '.join([ '[{}]'.format(x['name']) for x in params[shader_name] ])
 	header.append('xs {} {}'.format(shader_name, param_strings))
 
+	# Append headers
 	if len(params[shader_name]) > 0:
 		header.append('')
 		header.append('xs_begin')
@@ -53,13 +57,16 @@ for shader_name in params.keys():
 	header_text = '\n'.join([ '// {}'.format(x) for x in header ])
 	shader_path = shader_name.replace('/', path.sep)
 
+	# Read shader file
 	with open(path.join(DIR, 'shader', "{}.txt".format(shader_path)), 'r') as f:
 		shader = f.readlines()
 
+	# Write updated shader file
 	with open(path.join(DIR, 'shader', "{}.txt".format(shader_path)), 'w', newline="\n") as f:
 		has_comment = shader[0].startswith('//')
 		shader_lines = []
 
+		# Strip lines of whitespace
 		for line in shader:
 			if has_comment and line.startswith('//'):
 				continue
@@ -70,6 +77,7 @@ for shader_name in params.keys():
 		shader_text = header_text + '\n' + '\n'.join(shader_lines)
 		shader_text = '\n'.join([line.rstrip() for line in shader_text.splitlines()])
 
+		# Fix global variables which change between releases of MagicaVoxel
 		for old, new in FIX_GLOBAL_VARS.items():
 			shader_text = shader_text.replace(old, new)
 
