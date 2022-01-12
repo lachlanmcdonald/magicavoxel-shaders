bfcfe159ffb13168bc27f8b91c65d34753351387
Lachlan McDonald <lachlan.mcdonald@rga.com>
2022-01-12 23:09:42 +0000

    RF

--- package.sh
+++ package.sh
@@ -1,2 +1,5 @@
+# Copyright (c) 2022 Lachlan McDonald
+# This work is licensed under the MIT License (MIT)
+# https://github.com/lachlanmcdonald/magicavoxel-shaders
 TAG=$(git describe --tags)
 zip "magicavoxel-shaders-${TAG}.zip" shader/*.txt shader/**/*.txt
